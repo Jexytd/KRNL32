19f40c80aefa412aae69f9a7ba113accd2287e68
Jexytd <naufalwafi0104@gmail.com>
2021-12-17 18:59:22 +0700

    updating farm method (blox fruit)

--- Game/Blox Fruit/source.lua
+++ Game/Blox Fruit/source.lua
@@ -4,46 +4,161 @@ if type(ENGINE_l) ~= 'table' then
 end
 
 local Players = game:GetService('Players')
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
 local Client = Players.LocalPlayer
 
 local Windows = ENGINE_l
-
--- Unminified code here https://github.com/Jexytd/KRNL32/blob/master/Module/tp.lua
--- Old version code: https://pastebin.com/raw/i9FKyjvf (Before changing)
-do
-    setreadonly(table, false)
-    --/ Change table.find '==' to string.match()
-    function table.find(t, val, startindex)
-        local startindex = startindex or 0
-        local key = nil
-        for i,v in pairs(t) do
-            if (type(i) == 'number' and startindex <= i) and (v:match(val) or v == val) then
-                key = i
-                break
-            elseif (type(i) == 'string' and i:match(val)) then
-                key = i
-                break
-            end
-        end
-        return key
-    end
-    setreadonly(table, true)
-    if not table.find(math, 'clamp') then function math.clamp(value,min,max) return (max <= value and max) or (value <= min and min) or (min <= value and value <= max and value); end; end
-    function sumsof(a,b)if type(a)~='number'then return end;if type(b)~='number'then return end;local c={}if a<0 and b<0 then local d=b<a and'sub'or a<b and'add'or a==b and'equal'table.insert(c,d)if d=='sub'then local e=a-b;local f=a-e;table.insert(c,e)elseif d=='add'then local e=b-a;local f=a+e;table.insert(c,e)elseif d=='equal'then table.insert(c,0)end elseif 0<=a and 0<=b then local d=b<a and'sub'or a<b and'add'or a==b and'equal'table.insert(c,d)if d=='sub'then local e=a-b;local f=a-e;table.insert(c,e)elseif d=='add'then local e=b-a;local f=a+e;table.insert(c,e)elseif d=='equal'then table.insert(c,0)end end;if a<0 and 0<=b then local d=a<b and'add'or a==b and'equal'table.insert(c,d)if d=='add'then local e=-a*2;local g=e;local f=a+e;local h=b-f;g=g+h;local i=a+g or f+h;table.insert(c,g)elseif d=='equal'then table.insert(c,0)end elseif 0<=a and b<0 then local d=b<a and'sub'or a==b and'equal'table.insert(c,d)if d=='sub'then local e=a*2;local g=e;local f=a-e;local h=f-b;g=g+h;local i=a-g or f-h;table.insert(c,g)elseif d=='equal'then table.insert(c,0)end end;return c end
-    function tp(a)local b=game:GetService('Players').LocalPlayer;local c=b.Character;local d=c.PrimaryPart or c:FindFirstChild('HumanoidRootPart')or c:FindFirstChildWhichIsA('BasePart')local e=math.huge;local f=CFrame.new(math.floor(a.X)+1,a.Y,math.floor(a.Z)+1)local g=false;repeat d=c.PrimaryPart or c:FindFirstChild('HumanoidRootPart')or c:FindFirstChildWhichIsA('BasePart')local h=d.Position;local i,j=unpack(sumsof(h.X,f.X))local k,l=unpack(sumsof(h.Z,f.Z))local m=math.clamp(j,-200,200)local n=math.clamp(l,-200,200)local o=Vector3.new(math.floor(h.X)+1,f.Y,math.floor(h.Z)+1)if i=='sub'then o=o-Vector3.new(m,0,0)elseif i=='add'then o=o+Vector3.new(m,0,0)end;if k=='sub'then o=o-Vector3.new(0,0,n)elseif k=='add'then o=o+Vector3.new(0,0,n)end;if i=='equal'and k=='equal'then g=true end;if not g then pcall(function()d.CFrame=CFrame.new(o.X,o.Y,o.Z)+Vector3.new(0,100,0)end)end;wait(0.5)until b:DistanceFromCharacter(Vector3.new(a.X,0,a.Z))<=500 or not d or g;local p=CFrame.new(a.p.X or a.X,a.p.Y or a.Y,a.p.Z or a.Z)d.CFrame=p*CFrame.new(0,30,0)end
-end
 
 xpcall(function()
     LFarm = false
     LFarm_Boss = false
     LBoss_Debounce = false
+    CQuest = ''
+    CQ2 = ''
+    TPAddition = 200
+    TPDebounce = 0.51
+    TPHeight = 100
+
+    local Lcheck;
 
     local t2 = Windows:NewTab('Main')
     do
         local s1 = t2:NewSection('Farms')
         function LevelFarm()
-            if not LBoss_Debounce then
-               
+            
+            local Level = Client:FindFirstChild('Data') and Client.Data:FindFirstChild('Level').Value
+            local Module = require(ReplicatedStorage:FindFirstChild('Quests'))
+            local QTdata = {}
+
+            for quest,tbl in pairs(Module) do
+                local i = 1
+                for _,data in pairs(tbl) do
+                    local reqlvl = data['LevelReq']
+                    local task = data['Task']
+                    if reqlvl <= Level then
+                        local tempData = {}
+                        tempData[1] = reqlvl;
+                        tempData[2] = quest;
+                        tempData[3] = i; 
+                        tempData[4] = {}
+                        for k,v in pairs(task) do
+                            tempData[4][1] = k;
+                            tempData[4][2] = v;
+                        end
+                        table.insert(QTdata, tempData)
+                    end
+                    i = i + 1
+                end
+            end
+
+            table.sort(QTdata, function(t1,t2) t1[1] < t2[1] end)
+
+            local Target = {}
+            local Iteration = 0
+            for i = #QTdata,1,-1 do
+                Iteration = i;
+                if LFarm_Boss and not LBoss_Debounce and (workspace.Enemies:FindFirstChild(QTdata[i][4][1]) or ReplicatedStorage:FindFirstChild(QTdata[i][4][1])) and QTdata[i][4][2] == 1 then
+                    if CQuest ~= QTdata[i][4][1] then CQuest = QTdata[i][4][1] end
+                    Target[1] = QTdata[i][4][1]
+                    LBoss_Debounce = true
+                    break
+                end
+                if LFarm_Boss and not LBoss_Debounce and QTdata[i][4][2] ~= 1 then
+                    if CQuest ~= QTdata[i][4][1] then CQuest = QTdata[i][4][1] end
+                    Target[1] = QTdata[i][4][1]
+                    break
+                end
+                if not LFarm_Boss and QTdata[i][4][2] ~= 1 then
+                    if CQuest ~= QTdata[i][4][1] then CQuest = QTdata[i][4][1] end
+                    Target[1] = QTdata[i][4][1]
+                    break
+                end
+            end
+
+            if LFarm_Boss and not Lcheck then
+                Lcheck = game:GetService('RunService').RenderStepped:Connect(function()
+                    if LBoss_Debounce then
+                        if not workspace.Enemies:FindFirstChild(Target[1]) or not ReplicatedStorage:FindFirstChild(Target[1]) then
+                            LBoss_Debounce = false
+                        end
+                    end
+                    if not LBoss_Debounce or not LFarm or not LFarm_Boss then
+                        Lcheck:Disconnect()
+                    end
+                end)
+            end
+
+            local function get(name)
+                local found = false
+                local odist = math.huge
+                local instance
+        
+                for _,mob in pairs(workspace.Enemies:GetChildren()) do
+                    local newname = mob.Name:gsub('[Lv. %d+]', ''):gsub('[[]]', '')
+                    if mob:IsA('Model') and newname == name and mob:FindFirstChild('HumanoidRootPart') and mob:FindFirstChild('Humanoid') and mob.Humanoid.Health > 0 then
+                        local ndist = Client:DistanceFromCharacter(mob.HumanoidRootPart.Position)
+                        if ndist < odist then
+                            found = true
+                            instance = mob
+                            odist = ndist
+                            break
+                        end
+                    end
+                end
+        
+                if not found then
+                    for _,mob in pairs(game:GetService('ReplicatedStorage'):GetChildren()) do
+                        local newname = mob.Name:gsub('[Lv. %d+]', ''):gsub('[[]]', '')
+                        if mob:IsA('Model') and newname == name and mob:FindFirstChild('HumanoidRootPart') and mob:FindFirstChild('Humanoid') and mob.Humanoid.Health > 0 then
+                            local ndist = Client:DistanceFromCharacter(mob.HumanoidRootPart.Position)
+                            if ndist < odist then
+                                found = true
+                                instance = mob
+                                odist = ndist
+                                break
+                            end
+                        end
+                    end
+                end
+        
+                return instance
+            end
+
+            pcall(function()
+                if CQuest and CQ2 == '' then
+                    CQ2 = CQuest
+                    ReplicatedStorage.Remotes.CommF_:InvokeServer('StartQuest', QTdata[Iteration][2], QTdata[Iteration][3])
+                end
+            end)
+
+            local Target = get(Target[1])
+            local TP = loadstring(game:HttpGet('https://raw.githubusercontent.com/Jexytd/KRNL32/master/Module/tp.lua', true))()
+            if Target then
+                repeat
+                    local primary = Target:FindFirstChild('HumanoidRootPart')
+                    local height = 30
+                    local ratio = Client:DistanceFromCharacter(primary.Position)
+                    if ratio > 500 then tp(primary.CFrame, TPAddition, TPHeight, height, TPDebounce) end
+                    if ratio <= 500 then
+                        primary.Size = Vector3.new(50,60,50)
+                        pcall(function() 
+                            Client.Character.HumanoidRootPart.CFrame = primary.CFrame * CFrame.new(0,height,0)
+                        end)
+                        wait(os.clock()/os.time())
+                        pcall(function()
+                            if Client.Character:FindFirstChildOfClass('Tool') then
+                                local vu = game:GetService('VirtualUser')
+                                vu:Button1Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+                                vu:Button1Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+                            end
+                        end)
+                    end
+                until not LFarm or not Target or not Target.Parent or Target:FindFirstChildOfClass('Humanoid').Health <= 0
+                pcall(function()
+                    if not Client.PlayerGui.Main.Quest.Visible then
+                        C2 = ''
+                    end
+                end)
             end
         end
 
@@ -60,7 +175,16 @@ xpcall(function()
 
         s1:NewToggle('Include Boss', 'When boss quest available', function(t)
             LFarm_Boss = t
-            LFarm_table[2] = LFarm_Boss
+        end)
+        s1:NewLabel('When tp addition get high increase, please increase the debounce too or you will get tp\'ed back')
+        s1:NewSlider("TP Addition", "Teleport (num) away from current distance per debounce", 900, 1, function(s)
+            TPAddition = s
+        end)
+        s1:NewSlider("TP Debounce", "Increasing this will slowing the tp", 2, 0.1, function(s)
+            TPDebounce = s
+        end)
+        s1:NewSlider('TP Height', 'Set height of tp when in progress/working', 1000, -200, function(s)
+            TPHeight = s
         end)
     end
 end, function(e)
